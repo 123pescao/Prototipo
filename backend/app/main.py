f853af64d73b6b0d5c4ba7500102d3a3ec423df8
Joel <joel.caban2017@gmail.com>
2025-01-16 14:48:21 -0400

    db connection config

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
 #Entry point for server
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///watchly.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route('/status', methods=['GET'])
