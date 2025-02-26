3f17322d63b012db5f1c883d88d93eb6acfd5636
Joel <joel.caban2017@gmail.com>
2025-02-26 17:01:31 -0400

    PostgreSQL

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,5 +9,5 @@ DB_PATH = os.path.join(BASE_DIR, "instance", "watchly.db")
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", f"sqlite:///{DB_PATH}")
-    SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
-    FLASK_DEBUG = os.getenv("FLASK_DEBUG", True)
+    SENDGRID_API_KEY = os.getenv("SENDGRID_DEV_API_KEY")
+    FLASK_DEBUG = os.getenv("FLASK_DEBUG", "False").lower() in ("true", "1")
